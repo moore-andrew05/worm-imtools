cf213323895ccf9b85069f2bbdea70e49dff77fe
moore-andrew05 <andrew05@colostate.edu>
2024-01-10 15:51:16 -0700

    Changing naming to be consistent between github and PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wormimtools',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     author='Andrew Moore',
     author_email='moore.andrew0598@gmail.com',
