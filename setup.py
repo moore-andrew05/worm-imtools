bac82f4e290a25ea3663396a6c91fbecdc4a923e
moore-andrew05 <andrew05@colostate.edu>
2024-05-07 16:15:56 -0600

    Cleaned up a few last things, finished up Processor class. Final version before I graduate, v1.0.0 :). FYI: Def shouldn't be 1.0, not that clean.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wormimtools',
-    version='0.3.2',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=["numpy", "scipy", "matplotlib", "pandas", "scikit-image"],
     author='Andrew Moore',
